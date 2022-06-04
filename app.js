15048c93f33847ea7eb757c7bb2738fb5cc38b29
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-06-04 22:40:21 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const SocketIO = require("socket.io")(Http, {
+    cors: {
+        origin: '*'
+    }
+});
+
+let players = [];
+
+Http.listen(3001, () => {
+    console.log("Listening at :3001...");
+});
+
+function checkAndReveal() {
+    let reveal = true;
+    players.forEach(el => { if (el.vote === 0) reveal = false; });
+    if (reveal) SocketIO.emit("reveal", players);
+}
+
+SocketIO.on("connection", socket => {
+    socket.on("join", data => {
+        if (!players.find(el => el.name === data)) {
+            let player = {name: data, vote: 0};
+            players.push(player);
+            SocketIO.emit("newPlayer", player);
+        }
+        SocketIO.emit("players", players);
+
+        checkAndReveal()
+    })
+    socket.on("vote", data => {
+        let index = players.findIndex(el => el.name === data.name);
+        if (index !== -1) {
+            players[index] = data;
+            SocketIO.emit("players", players);
+        }
+
+        checkAndReveal()
+    });
+    socket.on("reset", data => {
+        players = [];
+        SocketIO.emit("reset", players);
+    });
+});
